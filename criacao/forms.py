74b937179551ac6b21b2a11de0ae44473e9c0c47
Nathan Aguiar Carvalho <nathan_ac@usp.br>
2021-12-15 18:56:03 -0300

    Criacao de gado por transicao de compra e alteracao do campo vendido por venda

--- criacao/forms.py
+++ criacao/forms.py
@@ -39,12 +39,12 @@ class PesosCreateForm(forms.Form):
 class TransacaoCreateForm(ModelForm):
     class Meta:
         model = transacao
-        fields = ['valor', 'envolvido', 'data', 'tags', 'observacoes','tipo']
+        fields = ['valor', 'envolvido', 'data', 'observacoes','tipo','gados']
         labels = {'valor':"Valor da transação", 
         'envolvido':"Envolvido", 
-        'data':"Data da transição:", 
-        'tags':"Identificação da(s) cabeça(s) de gado:",
-        'observacoes':"Observações:"
+        'data':"Data da transição:",
+        'observacoes':"Observações:",
+        'gados':"Insira as informações dos gados, dividindo cada gado por linha e cada atributo do gado por ',', coloque os atributos na ordem n_etiqueta, nascimento(YYYY-MM-DD), sexo, tipo, brinco_id se for compra. No caso de venda, preencha com n_etiqueta/n_brinco",
         }
         widgets = {
             'data':DateInput(),
