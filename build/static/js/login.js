c42bc29d397f97b9028e8085aff5bd03d195edae
ThiagoBrawerman <thiago.brawerman@gmail.com>
2021-12-16 17:12:47 -0300

    LocalValidation and loading screen

--- build/static/js/login.js
+++ build/static/js/login.js
@@ -1,1 +1,63 @@
-var toggle = false
+
+window.onload = function(){
+    const username = document.getElementById('username')
+    const password = document.getElementById('password')
+    
+}
+    
+function LocalValidationU(){
+    CheckInputsU();
+}
+
+function LocalValidationP(){
+    CheckInputsP();
+}
+
+function CheckInputsU (){
+    const usernamevalue = username.value.trim()
+    
+    if (usernamevalue == ''){
+        setErrorFor(username)
+        return false
+    } else{
+        setSuccessFor(username)
+        return true
+    }
+}
+
+function CheckInputsP (){
+    const passwordvalue = password.value.trim()
+    
+    if (passwordvalue == ''){
+        setErrorFor(password)
+        return false
+    } else{
+        setSuccessFor(password)
+        return true
+    }
+}
+
+function setErrorFor(input){
+    const formGroup = input.parentElement;
+
+    formGroup.className = 'form-group error'
+}
+
+function setSuccessFor(input){
+    const formGroup = input.parentElement;
+
+    formGroup.className = 'form-group success'
+}
+
+
+function FormLocalValidation(){
+    const loading = document.getElementById('loadingScreen')
+    
+
+    if ((CheckInputsP()) && (CheckInputsU())){
+        console.log("Hi");
+        loading.style.display = 'flex';
+    }
+
+
+}
