18d97915b150e068ea68f3d59089c9e618a9bdbf
Nathan Aguiar Carvalho <nathan_ac@usp.br>
2021-12-19 17:06:33 -0300

    Deploy da aplicação

--- Gado_control/settings.py
+++ Gado_control/settings.py
@@ -144,3 +144,6 @@ CORS_ALLOW_CREDENTIALS = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
